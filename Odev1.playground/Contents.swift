e9bb3d25444a9da97e1c7c0ed51f71c5e14b1c30
Firdevsssyildizzz <128640413+Firdevsssyildizzz@users.noreply.github.com>
2023-09-18 14:49:26 +0300

    Add files via upload

---
+++ Odev1.playground/Contents.swift
@@ -0,0 +1,98 @@
+import UIKit
+
+var ilce = "Yenimahalle"
+var kita = "Avrupa"
+var faks = 6000
+var postaKodu = 06000
+var instaAdresi = "yildizfrdvs"
+var calistigiBolum = "Bilgisayar Mühendisi"
+var urunMiktari = 10
+var musteriSoyAdi = "Yıldız"
+var odemeMiktari = 3000
+var dogumTarihi = "20/01/1998"
+var borc = 300
+var medeniHali = "Bekar"
+var videoYorumu = "Harika"
+var odemeSaati = 10
+var eftMiktari = 100000
+var satilanMiktar = 500
+var telefonModeli = "8 plus"
+var dergiAdi = "Avrupa Yakası"
+var basımTarihi = 20/01/2023
+var zamMiktari = 1000
+var daireSayisi = 5
+var enlem = 5.10
+var boylam = 5.13
+var yemekAdi = "pizza"
+var urunFiyati = 5000
+var sirket = "Bebiş Yoga"
+var videoAdi = "Firdevs Dünyası"
+var muzikSuresi = 5
+var mekanPuani = 5
+var dosyaADi = "bebiş firi"
+var resimFormati = "jpeg"
+var renk = "lila"
+var renkKodu = 2007
+var pcModeli = "Macbook pro"
+var ekranBoyutu = 15.1
+var kullanimSuresi = 59
+var basinc = 5
+var etkinlikGunu = "20/01/2024"
+var odemeGunu = "20/01/2023"
+var yolculukCikisTarihi = "20/01/1998"
+var mahalleAdi = "Demetevler"
+var otobusHatti = 600
+var kullanilanDakika = 500
+var kargoTakipNo = 56778
+var kuponSuresi = 5
+var kuponKodu = 875743
+var faturaTarihi = "20/01/2022"
+
+print("İlçe: \(ilce)")
+print("kıta: \(kita)")
+print("faks: \(faks)")
+print("posta kodu: \(postaKodu)")
+print("instagram adresi: \(instaAdresi)")
+print("çalıştığı bölüm: \(calistigiBolum)")
+print("ürün miktarı: \(urunMiktari)")
+print("müşteri soyadı: \(musteriSoyAdi)")
+print("ödeme miktarı: \(odemeMiktari)")
+print("doğum tarihi: \(dogumTarihi)")
+print("borç: \(borc)")
+print("medeni hali: \(medeniHali)")
+print("video yorumu: \(videoYorumu)")
+print("ödeme saati: \(odemeSaati)")
+print("eft miktarı: \(eftMiktari)")
+print("satılan miktar: \(satilanMiktar)")
+print("telefon modeli: \(telefonModeli)")
+print("deri adı: \(dergiAdi)")
+print("basım tarihi: \(basımTarihi)")
+print("zam miktarı: \(zamMiktari)")
+print("daire sayısı: \(daireSayisi)")
+print("enlem: \(enlem)")
+print("boylam: \(boylam)")
+print("yemek adı: \(yemekAdi)")
+print("ürün fiyatı: \(urunFiyati)")
+print("şirket: \(sirket)")
+print("video adı: \(videoAdi)")
+print("müzik süresi: \(muzikSuresi)")
+print("mekan puanı: \(mekanPuani)")
+print("dosya adı: \(dosyaADi)")
+print("resim formatı: \(resimFormati)")
+print("renk: \(renk)")
+print("renk kodu: \(renkKodu)")
+print("bilgisayar modeli: \(pcModeli)")
+print("ekran boyutu: \(ekranBoyutu)")
+print("kullanım süresi: \(kullanimSuresi)")
+print("basınç: \(basinc)")
+print("etkinlik günü: \(etkinlikGunu)")
+print("ödeme günü: \(odemeGunu)")
+print("yolculuk çıkış tarihi: \(yolculukCikisTarihi)")
+print("mahalle adı: \(mahalleAdi)")
+print("otobüs hattı: \(otobusHatti)")
+print("kullanılan dakika: \(kullanilanDakika)")
+print("kargo takip no: \(kargoTakipNo)")
+print("kupon süresi: \(kuponSuresi)")
+print("kupon kodu: \(kuponKodu)")
+print("fatura tarihi: \(faturaTarihi)")
+
